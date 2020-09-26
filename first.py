214dbf776e79670dea2805e4de014653588da11b
sebastian1707 <39193924+sebastian1707@users.noreply.github.com>
2020-09-26 13:18:06 -0400

    Sebastian Facebook likes in JSON

--- first.py
+++ first.py
@@ -1,19 +1,17 @@
+#export PYTHONPATH=/opt/anaconda3/lib/python3.8/site-packages:$PYTHONPATH
+
+
 import facebook
-import requests 
-import urllib3
 import json
 
 
 
-token = "EAAnznRNVKZAkBAGvNuZB52CioeXKDUwOJUf4wFDxjMHwhWD8gWm6eIEpdVIiOmZAWVgOMgQ4LRiIoL3JAKQIPrRLfzLBV5DcRdlYp4iQ6ZANnqhXZBZC0kbn4sgYyEb3SerwxCll1N6LMaJ6MvAgqdEbyb75FkmeOqgRqg7KW2NMeP8uEnaEXKh9U9ZCrNxbtZAsD69CqBkcnZAZCZC2HqHQ2jMZBQCKmfMiBZCAZAtjpIyAlOLAZDZD"
+token = 'EAAFiVzBZCbZBsBAFdgnhhZBjfgDBcd4KN4RvpLEKHaawjLktPeqVAgY2E954wjYb1g5pRKyL9YyfrWXXbao2K5Cj9cWDiNfpD7XjWGDK0hucM6ZCV9biO7k9wPfpruHUdmZCEllFlkZBvNrL0y5vzt2kLsJMktlNSNMbiLzaG0ndVw02CQHEuZCihG8he5ZB4rl2qcfafOxAlzxEs80h76vyP1QKdqpBLccV3gQYELnhrgZDZD'
 
 
 def main():
 	graph = facebook.GraphAPI(token)
-	#fields = ['first_name', 'location{location}','email','link']
-	profile = graph.get_object('me',fields='likes')	
-	#return desired fields
-	print(json.dumps(profile, indent=4))
-
-if __name__ == '__main__':
-	main()
+	
+	fields = ['gender, likes']
+	
+	profile = graph.get_object('me', fields = fields)
