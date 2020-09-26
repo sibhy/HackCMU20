d30646a19ba08d90f9aca4a4ca1932eda0896bcb
Shizen <shizenliu@gmail.com>
2020-09-26 13:45:37 -0400

    Incorporated timeline

--- likesToWordCloud.py
+++ likesToWordCloud.py
@@ -5,7 +5,7 @@ import json
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 import pandas as pd 
-
+import timeline
 
 # token needs to be refreshed every two hours.
 token = "EAAnznRNVKZAkBANq54ebSXuosd3KatIT8goyVBZARoOXND5h4R8AEeb54TFQXn47yopZBaWbpMs02OcggaIXZBBwzc5rtbd7pxz42bimTnsgDZBgdkXv8sT0yyXOg0v3pZAi312tExihGdN2uJ1HxDQZADzgqrChly5nMLc9WZAdV646ZCAznTfo7qMw3WSvCKIePkZADyZB6mPFQZDZD"
@@ -14,18 +14,19 @@ def pullLikes():
     graph = facebook.GraphAPI(token)
     likes = graph.get_object('me', fields='likes')
     count = 0
-    
     # count the number of sets , may be wrong terminology, not used to dicts.
     for key in likes['likes']['data']:
         count += 1
     
     # create a new list with all the names of the things you've liked
     nameList = list()
+    dates = list()
     for i in range(count):
         nameList.append(likes['likes']['data'][i]['name'])
+        dates.append(likes['likes']['data'][i]['created_time'][:10])
         # add the names of each thing liked to the list
     
-    return nameList 
+    return nameList, dates
 
 def makeWordCloud(listy):
     #convert list to string and generate
@@ -40,7 +41,11 @@ def makeWordCloud(listy):
     
 
 def main():
-    makeWordCloud(pullLikes())
+    nameList, dates = pullLikes()
+    makeWordCloud(nameList)
+    timeline.makeTimeLine(nameList[:10], dates[:10])
+    timeline.makeTimeLine(nameList[10:], dates[10:])
+
 
 if __name__ == '__main__':
 	main()
--- timeline.py
+++ timeline.py
@@ -4,84 +4,62 @@ Creating a timeline with lines, dates, and text
 ===============================================
 https://matplotlib.org/3.1.1/gallery/lines_bars_and_markers/timeline.html
 '''
-'''
-from matplotlib import rcParams
-rcParams['font.family'] = 'sans-serif'
-rcParams['font.sans-serif'] = ['Tahoma']
-'''
+
 
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.dates as mdates
 from datetime import datetime
 
-
-names = ['apple', 'pear', 'vw', 'CMU', '112', '42',
-            'foo!', 'bar', 'baz', 'bat']
-
-dates = ['2019-02-26', '2019-02-26', '2018-11-10', '2018-11-10',
-            '2018-09-18', '2018-08-10', '2018-03-17', '2018-03-16',
-            '2018-03-06', '2018-01-18']
-
-# Convert date strings to datetime
-# e.g. 2019-02-06 becomes datetime.datetime(2019, 2, 26, 0, 0)
-dates = [datetime.strptime(d, "%Y-%m-%d") for d in dates]
+def makeTimeLine(names, dates):
+    # Convert date strings to datetime
+    # e.g. 2019-02-06 becomes datetime.datetime(2019, 2, 26, 0, 0)
+    dates = [datetime.strptime(d, "%Y-%m-%d") for d in dates]
 
 
-##############################################################################
-# Next, we'll create a `~.Axes.stem` plot with some variation in levels as to
-# distinguish even close-by events. In contrast to a usual stem plot, we will
-# shift the markers to the baseline for visual emphasis on the one-dimensional
-# nature of the time line.
-# For each event, we add a text label via `~.Axes.annotate`, which is offset
-# in units of points from the tip of the event line.
-#
-# Note that Matplotlib will automatically plot datetime inputs.
+    # Choose some nice levels
+    levels = np.tile([-5, 5, -4, 4, -3, 3, -2, 2, -1, 1],
+                    int(np.ceil(len(dates)/6)))[:len(dates)]
+
+    # Create figure and plot a stem plot with the date
+    fig, ax = plt.subplots(figsize=(8.8, 4), constrained_layout=True)
+    ax.set_title("When did you like ___?", fontname='Comic Sans MS', fontsize=18, 
+                    color = 'blue', fontweight = 'bold')
+
+    # ax.stem creates a stem plot
+    '''
+    Edit linefmt to change color and line style
+    '-' solid line
+    '--' dashed line
+    '-.' dash-dot line
+    ':' dotted line
+    Colors: matplotlib.org/users/dflt_style_changes.html
+    '''
+    markerline, stemline, baseline = ax.stem(dates, levels,
+                                            linefmt="C1:", basefmt="k-",
+                                            use_line_collection=True)
+
+    plt.setp(markerline, mec="k", mfc="w", zorder=5)
 
 
-# Choose some nice levels
-levels = np.tile([-5, 5, -3, 3, -1, 1],
-                 int(np.ceil(len(dates)/6)))[:len(dates)]
+    # Shift the markers to the baseline by replacing the y-data by zeros.
+    markerline.set_ydata(np.zeros(len(dates)))
 
-# Create figure and plot a stem plot with the date
-fig, ax = plt.subplots(figsize=(8.8, 4), constrained_layout=True)
-ax.set_title("When did you like ___?", fontname='Comic Sans MS', fontsize=18, 
-                color = 'blue', fontweight = 'bold')
+    # annotate lines
+    vert = np.array(['top', 'bottom'])[(levels > 0).astype(int)]
+    for d, l, r, va in zip(dates, levels, names, vert):
+        ax.annotate(r, xy=(d, l), xytext=(-3, np.sign(l)*3),
+                    fontname = 'Comic Sans MS', fontsize = '6', textcoords="offset points", va=va, ha="right", color = 'grey')
 
-# ax.stem creates a stem plot
-'''
-Edit linefmt to change color and line style
-'-' solid line
-'--' dashed line
-'-.' dash-dot line
-':' dotted line
-Colors: matplotlib.org/users/dflt_style_changes.html
-'''
-markerline, stemline, baseline = ax.stem(dates, levels,
-                                         linefmt="C1:", basefmt="k-",
-                                         use_line_collection=True)
+    # format xaxis with 4 month intervals
+    ax.get_xaxis().set_major_locator(mdates.MonthLocator(interval=2))
+    ax.get_xaxis().set_major_formatter(mdates.DateFormatter("%b %Y"))
+    plt.setp(ax.get_xticklabels(), rotation=30, ha="right", fontname = 'Comic Sans MS')
 
-plt.setp(markerline, mec="k", mfc="w", zorder=3)
+    # remove y axis and spines
+    ax.get_yaxis().set_visible(False)
+    for spine in ["left", "top", "right"]:
+        ax.spines[spine].set_visible(False)
 
-
-# Shift the markers to the baseline by replacing the y-data by zeros.
-markerline.set_ydata(np.zeros(len(dates)))
-
-# annotate lines
-vert = np.array(['top', 'bottom'])[(levels > 0).astype(int)]
-for d, l, r, va in zip(dates, levels, names, vert):
-    ax.annotate(r, xy=(d, l), xytext=(-3, np.sign(l)*3),
-                fontname = 'Comic Sans MS', textcoords="offset points", va=va, ha="right", color = 'grey')
-
-# format xaxis with 4 month intervals
-ax.get_xaxis().set_major_locator(mdates.MonthLocator(interval=4))
-ax.get_xaxis().set_major_formatter(mdates.DateFormatter("%b %Y"))
-plt.setp(ax.get_xticklabels(), rotation=30, ha="right", fontname = 'Comic Sans MS')
-
-# remove y axis and spines
-ax.get_yaxis().set_visible(False)
-for spine in ["left", "top", "right"]:
-    ax.spines[spine].set_visible(False)
-
-ax.margins(y=0.1)
-plt.show()
+    ax.margins(y=0.1)
+    plt.show()
